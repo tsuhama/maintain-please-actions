1e226faf80879ef8d3f38f7cc29e10aab9ac6145
tsuhama <tsuyoshihamano94@gmail.com>
2025-04-12 17:15:44 +0200

    update dist

--- dist/maintenance-branch/index.js
+++ dist/maintenance-branch/index.js
@@ -25,3 +25,8 @@ function createGitOperations(inputs) {
     const [owner, repo] = repoUrl.split('/');
     return createGitHubControl(repo, owner, 'main', inputs.token);
 }
+if (require.main === module) {
+    main().catch((err) => {
+        core.setFailed(`release-please-please-me maintenance branch action failed: ${err.message}`);
+    });
+}
